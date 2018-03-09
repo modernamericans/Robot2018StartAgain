4f1583b976040159c3de078319da6c6efeadd051
modernamericans <35410521+modernamericans@users.noreply.github.com>
2018-03-09 09:32:43 -0800

    HopefullyFixed

--- src/org/usfirst/frc6442/Robot2018StartAgain/commands/Feed.java
+++ src/org/usfirst/frc6442/Robot2018StartAgain/commands/Feed.java
@@ -5,7 +5,7 @@ import org.usfirst.frc6442.Robot2018StartAgain.subsystems.Feeder;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class Feed extends InstantCommand{
-	protected Feeder feeder = Robot.feeder;
+	public Feeder feeder = Robot.feeder;
 	public Feed() {
 		requires(Robot.feeder);
 	}
