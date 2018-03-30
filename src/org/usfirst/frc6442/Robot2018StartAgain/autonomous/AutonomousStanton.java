f71bc4123708afa014e05fe715dd32745626a1f6
StanSchmitz <36393073+StanSchmitz@users.noreply.github.com>
2018-03-29 19:26:40 -0700

    Dang it Dev
    It was a good Idea

---
+++ src/org/usfirst/frc6442/Robot2018StartAgain/autonomous/AutonomousStanton.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc6442.Robot2018StartAgain.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc6442.Robot2018StartAgain.commands.*;
+
+public class AutonomousStanton extends CommandGroup {
+
+	public AutonomousStanton() {
+		addSequential(new TurnRightGyro(45));
+		while(true){
+			addSequential(new DriveStraightForwardTime(.5));
+			addSequential(new DriveStraightReverseTime(.5));
+			addSequential(new TurnLeftGyro(90));
+			addSequential(new DriveStraightForwardTime(.5));
+			addSequential(new DriveStraightReverseTime(.5));
+			addSequential(new TurnRightGyro(90));
+		}
+	}
+}
