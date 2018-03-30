85f0d491944e5ad4931b950d6c7246a3379e6336
StanSchmitz <36393073+StanSchmitz@users.noreply.github.com>
2018-03-29 19:17:45 -0700

    Display Auto's Ready

---
+++ src/org/usfirst/frc6442/Robot2018StartAgain/autonomous/AutonomousDance.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc6442.Robot2018StartAgain.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc6442.Robot2018StartAgain.commands.*;
+
+public class AutonomousDance extends CommandGroup {
+
+	public AutonomousDance() {
+		while(true) {
+			addSequential(new TurnLeftGyro(90));
+			addSequential(new TurnRightGyro(90));
+			addSequential(new TurnLeftGyro(90));
+			addSequential(new TurnRightGyro(90));
+			addSequential(new DriveStraightForwardTime(1));
+			addSequential(new DriveStraightReverseTime(1));
+		
+		}
+		
+		
+	}
+}
--- src/org/usfirst/frc6442/Robot2018StartAgain/autonomous/AutonomousDisplay.java
+++ src/org/usfirst/frc6442/Robot2018StartAgain/autonomous/AutonomousDisplay.java
@@ -7,23 +7,22 @@ public class AutonomousDisplay extends CommandGroup {
 
 	public AutonomousDisplay() {
 		
-		addSequential(new DriveForwardTime(3));
-		addSequential(new DriveStop(1));
-		addSequential(new TurnRightGyro(90));
-		addSequential(new DriveStop(1));
-		addSequential(new DriveForwardTime(.5));
-		addSequential(new DriveStop(1));
-		addSequential(new TurnLeftGyro(90));
-		addSequential(new DriveStop(1));
-		addSequential(new DriveReverseTime(3));
-		addSequential(new DriveStop(1));
-		addSequential(new TurnRightGyro(90));
-		addSequential(new DriveStop(1));
-		addSequential(new DriveReverseTime(.5));
-		addSequential(new DriveStop(1));
-		addSequential(new TurnLeftGyro(90));
-		addSequential(new DriveForwardTime(1.5));
-		addSequential(new DriveStop(1));
-		addSequential(new DriveReverseTime(1.5));
+		while(true){
+			addSequential(new DriveStraightForwardTime(3));
+			addSequential(new DriveStop(1));
+			addSequential(new TurnRightGyro(90));
+			addSequential(new DriveStop(1));
+			addSequential(new DriveStraightForwardTime(.5));
+			addSequential(new DriveStop(1));
+			addSequential(new TurnLeftGyro(90));
+			addSequential(new DriveStop(1));
+			addSequential(new DriveStraightReverseTime(3));
+			addSequential(new DriveStop(1));
+			addSequential(new TurnRightGyro(90));
+			addSequential(new DriveStop(1));
+			addSequential(new DriveStraightReverseTime(.5));
+			addSequential(new DriveStop(1));
+			addSequential(new TurnLeftGyro(90));
+		}
 	} 
 }
