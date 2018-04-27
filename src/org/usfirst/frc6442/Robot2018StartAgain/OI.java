21f4024b4d4877ccdcdd2c43ac4e6d573ff867ee
modernamericans <35410521+modernamericans@users.noreply.github.com>
2018-04-27 09:30:52 -0700

    got it

--- src/org/usfirst/frc6442/Robot2018StartAgain/OI.java
+++ src/org/usfirst/frc6442/Robot2018StartAgain/OI.java
@@ -28,19 +28,19 @@ public class OI {
 	}
 //Map buttons
 	public void defineButtons() {
-	//	Controller1.Y.whileHeld(newLaunchCubeHigh()());
+		Controller1.Y.whileHeld(new LaunchCubeHigh());
 		Controller2.Y.whenPressed(new LaunchCubeHigh());
 		
-		//Controller1.X.whenPressed(new LauncherDown());
-		//Controller1.B.whenPressed(new LauncherUp());
+		Controller1.X.whenPressed(new LauncherDown());
+		Controller1.B.whenPressed(new LauncherUp());
 		Controller2.X.whenPressed(new LauncherDown());
 		Controller2.B.whenPressed(new LauncherUp());
 		
-		//Controller1.A.whenPressed(new LaunchCubeLow());
+		Controller1.A.whenPressed(new LaunchCubeLow());
 		Controller2.A.whenPressed(new LaunchCubeLow());
 		
-		Controller2.LB.whileHeld(new BarLower());
-		Controller2.RB.whileHeld(new BarRaise());
+		Controller1.LB.whileHeld(new BarLower());
+		Controller1.RB.whileHeld(new BarRaise());
 			
 
 	}
@@ -73,15 +73,15 @@ public class OI {
 	}
 //Update Feeder Method
 	private void UpdateFeederValue() {
-		double RT = Controller2.axisRT();
-		double LT = Controller2.axisLT();
+		double RT = Controller1.axisRT();
+		double LT = Controller1.axisLT();
 		feed = RT-LT;
 		launch = -LT/2;
 	}
 //updater grabber method
 	private void UpdateGabberValue() {
-		double RT = Controller2.axisRT();
-		double LT = Controller2.axisLT();
+		double RT = Controller1.axisRT();
+		double LT = Controller1.axisLT();
 			grabLeft = RT-LT;
 			grabRight = RT-LT;
 	}
